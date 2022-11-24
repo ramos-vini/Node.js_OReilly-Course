d12daec0067f5527a0da4635580c84454dc6bf52
ramos-vini <ramoscontrole@gmail.com>
2022-11-24 19:59:41 -0300

    Understanding Requests

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
     // process.exit();
 });
 
