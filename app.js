97b787a50c74e83e7551639ed20c80409e0c4fef
ramos-vini <ramoscontrole@gmail.com>
2022-11-24 20:24:44 -0300

    Routing Requests

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
+
+    if(req.url === "/"){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Send a Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My Node.js Server</title></head>');
