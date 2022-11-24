f11c31b04cae039679b3318b9349009aecb42fc4
ramos-vini <ramoscontrole@gmail.com>
2022-11-24 20:36:21 -0300

    Redirecting Requests

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const http = require('http');
+const fs = require ('fs');
 
 const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
 
-    if(req.url === "/"){
+    if(url === "/"){
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Send a Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+
+    if(url === "/message" && method === "POST"){
+        fs.writeFileSync('message.txt', 'Here comes a message!');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
 
