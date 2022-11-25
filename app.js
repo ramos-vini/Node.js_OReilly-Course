406b4dc0418fe7c8543e701b57a0f90a8ba3bb4e
ramos-vini <ramoscontrole@gmail.com>
2022-11-25 13:08:51 -0300

    Parsing Request Bodies

--- app.js
+++ app.js
@@ -15,7 +15,19 @@ const server = http.createServer((req, res) => {
     }
 
     if(url === "/message" && method === "POST"){
-        fs.writeFileSync('message.txt', 'Here comes a message!');
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
