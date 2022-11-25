1476f70eb0d1aed3435b737c4a4b9bccc381c8c4
ramos-vini <ramoscontrole@gmail.com>
2022-11-25 13:21:17 -0300

    Understanding Event Driven Code Execution

--- app.js
+++ app.js
@@ -21,16 +21,15 @@ const server = http.createServer((req, res) => {
             body.push(chunk);
         });
 
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             console.log(parsedBody);
             const message = parsedBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
         });
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
 
     res.setHeader('Content-Type', 'text/html');
