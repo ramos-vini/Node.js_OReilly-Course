607af6d19ae2d5e321de3d6bd56ba663a2f3831e
ramos-vini <ramoscontrole@gmail.com>
2023-02-23 16:50:16 -0300

    Returning a 404 Page

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const path = require('path');
 
 const bodyParser = require('body-parser');
 
@@ -12,7 +15,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
