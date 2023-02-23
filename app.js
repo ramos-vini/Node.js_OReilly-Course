22ec568a1fe3acba700a94136ad638f354a30425
ramos-vini <ramoscontrole@gmail.com>
2023-02-23 16:25:06 -0300

    Creating HTML Pages

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found');
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
