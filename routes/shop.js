8342554f957a2d95b068e9330df5fc61bb2da16b
ramos-vini <ramoscontrole@gmail.com>
2023-02-23 16:40:40 -0300

    Serving HTML Pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const path = require('path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello, Express.js!</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
