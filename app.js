7f9a0092fc9d765e168861bbbb385f293e0c0be8
ramos-vini <ramoscontrole@gmail.com>
2023-02-23 18:12:09 -0300

    Serving Files Statically

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
