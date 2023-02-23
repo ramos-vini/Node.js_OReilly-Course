6c2f4f1ca2981dd84ea6b4b60ca45faa9c3c6e83
ramos-vini <ramoscontrole@gmail.com>
2023-02-23 17:04:25 -0300

    Using a Helper Function for Navigation

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 });
 
 module.exports = router;
