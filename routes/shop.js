ae0690b2b4aaca53cb0487f9ce6919b3c476a35f
ramos-vini <ramoscontrole@gmail.com>
2023-03-01 17:25:52 -0300

    Outputting Dynamic Content

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-const path = require('path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('shop');
+    res.render('shop', {prods: adminData.products, title: 'Pug Shop'});
 });
 
 module.exports = router;
